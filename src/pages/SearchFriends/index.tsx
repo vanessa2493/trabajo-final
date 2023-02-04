6f84d031e7ca40f297b69ceb04e21837f5e323b9
vanessa2493 <vaneira24@gmail.com>
2023-02-04 18:22:08 -0300

    Create Search Friends Page

---
+++ src/pages/SearchFriends/index.tsx
@@ -0,0 +1,12 @@
+import {Layout} from "../../components";
+import React from "react";
+
+const SearchFriends = () => {
+    return (
+        <Layout page={'SearchFriends'}>
+            <div>Search Friends</div>
+        </Layout>
+    )
+}
+
+export {SearchFriends};
