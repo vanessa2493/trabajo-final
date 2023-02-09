d4a3e70ba1fd899c4b4ea131dff710f7b37f44eb
vanessa2493 <vaneira24@gmail.com>
2023-02-09 07:28:24 -0300

    add hamburger menu bg color, quit outlet, add api movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { Outlet } from "react-router-dom";
-
 const App = () => {
   return (
       <div>
-          <Outlet/>
       </div>
   );
 };
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,8 +3,15 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'https://conectadas-app-default-rtdb.firebaseio.com/',
     params: {
-        api_key: "prueba-api-key"
+        api_key: "AIzaSyAekPyTLVZ_sfmMG3Jwow7NLM3AsE0NRbY"
     }
 })
 
-export {api}
+const apiMovies = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        api_key : "0bce2b3619ac231ad6ce364b6d8015ab"
+    }
+})
+
+export { api, apiMovies }
