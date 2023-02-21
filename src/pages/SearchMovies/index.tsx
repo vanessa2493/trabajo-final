c42a21488b96580efe97a9d964226fef37ca3f9f
vanessa2493 <vaneira24@gmail.com>
2023-02-21 10:49:36 -0300

    Quit search movies button and be replaced by an onchange event.

--- src/pages/SearchMovies/index.tsx
+++ src/pages/SearchMovies/index.tsx
@@ -1,6 +1,6 @@
 import {Layout} from "../../components";
 import React from "react";
-import {Button, Form, FormControl} from "react-bootstrap";
+import {Form, FormControl} from "react-bootstrap";
 import {useForm} from "react-hook-form";
 import {useSearchParams} from "react-router-dom";
 import {MoviePayload} from "../../types";
@@ -11,38 +11,30 @@ const SearchMovies = () => {
     const {register, handleSubmit} = useForm<MoviePayload>()
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const onSubmit = (data: MoviePayload) => {
+    const handleSearch = (data: MoviePayload) => {
         serviceMovies.search(data.query, data.page);
     }
 
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const queryParams = Object.fromEntries(searchParams.entries());
-        queryParams.query = e.target.value;
-        setSearchParams(new URLSearchParams(queryParams));
-    }
+    const onInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const value = event.currentTarget.value;
+        setSearchParams({
+            ...searchParams,
+            query: value,
+            page: '1', // Restablecer la página a 1 al cambiar la consulta
+        });
+    };
 
     return (
         <Layout page={'SearchMovies'}>
-            <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form onChange={handleSubmit(handleSearch)}>
                 <FormControl
                     type="text"
                     placeholder="Search"
                     className="mr-sm-2"
                     {...register('query')}
                     value={searchParams.get('query') ?? ''}
-                    onChange={handleSearch}
+                    onInput={onInputChange}
                 />
-                {/*<FormControl*/}
-                {/*    type="number"*/}
-                {/*    placeholder="Page"*/}
-                {/*    className="mr-sm-2"*/}
-                {/*    {...register('page')}*/}
-                {/*    value={searchParams.get('page') ?? '1'}*/}
-                {/*    onChange={(e) => {*/}
-                {/*        setSearchParams({...searchParams, page: e.target.value});*/}
-                {/*    }}*/}
-                {/*/>*/}
-                <Button variant="outline-success" type="submit">Search</Button>
             </Form>
         </Layout>
     )
