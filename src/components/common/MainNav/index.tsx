60c0e6322dafa4cbda1fb4560c1a1ffd15a252cb
vanessa2493 <vaneira24@gmail.com>
2023-03-13 20:36:30 -0300

    logout

--- src/components/common/MainNav/index.tsx
+++ src/components/common/MainNav/index.tsx
@@ -2,51 +2,31 @@ import { FC } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import "./styles.scss"
+import { useAuth } from "../../../hooks";
 
 type Props = {
   hideNav?: boolean;
 };
 
 const MainNav: FC<Props> = ({ hideNav }) => {
-  const handleLogout = () => {
-    //logout();
-    console.log('logout')
-    //navigate('/login'); // Redireccionar a la página de login después del logout
+  const navigate = useNavigate();
+  const { currentUser, logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login');
   };
 
   return (
       <Navbar className="main-nav" expand="lg">
-        <Container fluid className="container-nav" >
+        <Container fluid className="container-nav">
           <Navbar.Brand href='/'>
             <h1>ConectADAs</h1>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {hideNav ? (
-                  <>
-                    <NavLink className="nav-link" to={"/"}>
-                      Home
-                    </NavLink>
-                    <NavLink className="nav-link" to={"/searchMovies"}>
-                      Buscar Peliculas
-                    </NavLink>
-
-                    <NavLink className="nav-link" to={"/searchFriends"}>
-                      Buscar Amigxs
-                    </NavLink>
-
-                    <NavDropdown title="Usuario" id="basic-nav-dropdown">
-                      <NavLink className="nav-link" to={"/profile"}>
-                        Profile
-                      </NavLink>
-                      <NavDropdown.Divider />
-                      <NavLink className="nav-link" to={"/login"} onClick={handleLogout}>
-                        Logout
-                      </NavLink>
-                    </NavDropdown>
-                  </>
-              ) : (
+              {!currentUser && (
                   <>
                     <NavLink className="nav-link" to={"/login"}>
                       Ingresar
@@ -54,6 +34,28 @@ const MainNav: FC<Props> = ({ hideNav }) => {
                     <NavLink className="nav-link" to={"/signup"}>
                       Registrarse
                     </NavLink>
+                  </>
+              )}
+              {currentUser && (
+                  <>
+                    <NavLink className="nav-link" to={"/"}>
+                      Home
+                    </NavLink>
+                    <NavLink className="nav-link" to={"/searchMovies"}>
+                      Buscar Peliculas
+                    </NavLink>
+                    <NavLink className="nav-link" to={"/searchFriends"}>
+                      Buscar Amigxs
+                    </NavLink>
+                    <NavDropdown title={currentUser.displayName} id="user-dropdown">
+                      <NavLink className="nav-link" to={"/profile"}>
+                        Profile
+                      </NavLink>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item onClick={handleLogout}>
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
                   </>
               )}
             </Nav>
